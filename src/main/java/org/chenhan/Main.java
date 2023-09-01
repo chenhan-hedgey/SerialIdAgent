a4b539a1777df0e7ac79ebad577adea75a27459f
chenhan <chenhan.tin@foxmail.com>
2023-09-01 10:03:15 +0800

    init:初始化仓库

---
+++ src/main/java/org/chenhan/Main.java
@@ -0,0 +1,13 @@
+package org.chenhan;
+
+/**
+ * @Author: chenhan
+ * @Description: ${DESCRIPTION}
+ * @ProjectName: Default (Template) Project
+ * @Date: 2023/9/1 9:31
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
