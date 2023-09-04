a55435b07f9f4254822b0b56b15520fecab50104
chenhan <chenhan.tin@foxmail.com>
2023-09-04 14:14:04 +0800

    add:增加ObjectUtils中的isAllNull方法

--- src/main/java/org/chenhan/serialAgent/util/ObjectUtils.java
+++ src/main/java/org/chenhan/serialAgent/util/ObjectUtils.java
@@ -37,5 +37,28 @@ public class ObjectUtils {
         return false;
     }
 
+    /**
+     * 判断多个对象实例是否都为null。
+     *
+     * @param objects 要检查的多个对象
+     * @return 如果多个对象实例都为null，则返回true，否则返回false
+     */
+    public static boolean isAllNull(Object... objects) {
+        // 如果传入的数组为null，直接返回true
+        if (objects == null) {
+            return true;
+        }
+
+        // 遍历对象数组，只要有一个对象不为null就返回false
+        for (Object object : objects) {
+            if (!isNull(object)) {
+                return false;
+            }
+        }
+
+        // 所有对象都为null，返回true
+        return true;
+    }
+
 }
 
